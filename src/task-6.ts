c544fe689800ea9e79c63c16b71ecf90c0b8be6a
kevin <kalnikkevin@gmail.com>
2025-08-04 21:23:55 +0300

    change

--- src/task-6.ts
+++ src/task-6.ts
@@ -2,6 +2,6 @@ function getFirstElement<T>(arr: T[]): T {
     return arr[0];
   }
   
-  getFirstElement<Number>([1, 2, 3]); // 1
+  getFirstElement<number>([1, 2, 3]); // 1
   getFirstElement<string>(["a", "b", "c"]); // "a"
   getFirstElement<boolean>([true, false, true]); // true
